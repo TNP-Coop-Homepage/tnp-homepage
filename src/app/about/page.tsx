25ad0b6042ef111777ade3b953998b1cddae35e0
Reiru329411 <running941293@gmail.com>
2024-02-20 23:16:55 +0900

    Add base of tsx
    tsxを書くための基礎部分を追加

---
+++ src/app/about/page.tsx
@@ -0,0 +1,7 @@
+import styles from "./page.module.css"
+export default async function About() {
+    return <div>
+        <h1 className={styles.body}>Hello World!</h1>
+        <p className={styles.body}>hi</p>
+    </div>
+}
