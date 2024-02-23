c1460635ab9aa6fe5561974b74949faa4955b45a
Reiru329411 <running941293@gmail.com>
2024-02-24 01:36:32 +0900

    Create base of about tsx
    Aboutページの構成の基礎のtsxを作成

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,23 @@
 import styles from "./page.module.css"
 export default async function About() {
     return <div>
-        <h1 className={styles.body}>Hello World!</h1>
-        <p className={styles.body}>hi</p>
+        <title>About</title>
+            <h1 className={styles.AboutTitle}>About</h1>
+            <div className={styles.IndexText}>
+                <h2 className={styles.IndexHeader}>目次</h2>
+                <p>
+                    サンプルテキストです．こんなもん？
+                </p>
+            </div>
+            <div className={styles.BodyText}>
+                <h2 className={styles.BodyHeader}>見出し2</h2>
+                <p>
+                    段落1
+                </p>
+                <h2 className={styles.BodyHeader}>見出し3</h2>
+                <p>
+                    段落1
+                </p>
+            </div>
     </div>
 }
