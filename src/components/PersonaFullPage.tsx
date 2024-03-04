353f62a376bb50362bdca91122b0eb342528654d
℃ (せし) <76618755+NitCelcius@users.noreply.github.com>
2024-03-04 10:41:24 +0900

    Separate persona-common stylesheet
    Persona コンポーネントに共通する CSS を隔離しました

--- src/components/PersonaFullPage.tsx
+++ src/components/PersonaFullPage.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import {Persona} from "@/types/Persona";
-import styles from "./PersonaFullPage.module.css";
+import personaStyles from "./PersonaShared.module.css";
+import localStyles from "./PersonaFullPage.module.css";
 
 export default function PersonaFullPage(persona: Persona) {
     return (
             <main style={{'--primaryColor': persona.primaryColor} as React.CSSProperties}>
-                <div className={styles.PersonaAreaWrapper}>
+            <main style={{'--primaryColor': persona.primaryColor, '--secondaryColor': persona.secondaryColor, '--backgroundColor': persona.backgroundColor } as React.CSSProperties}>
+                <div className={localStyles.PersonaAreaWrapper}>
                     <img src={persona.iconSrc} alt={persona.displayName} width={128} height={128}/>
-                    <h2 className={styles.DisplayName}>{persona.displayName}</h2>
+                    <h2 className={localStyles.DisplayName}>{persona.displayName}</h2>
                 </div>
                 <p>{persona.description}</p>
             </main>
---
+++ src/components/PersonaPrevBanner.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import {Persona} from "@/types/Persona";
+import styles from "./PersonaPrevBanner.module.css";
+import personaStyles from "./PersonaShared.module.css";
+
+import {MAX_PERSONA_DESC_LENGTH} from "@/components/MAX_PERSONA_DESC_LENGTH";
+
+export default function PersonaPrevBanner(persona: Persona) {
+    return (
+        <main style={{ '--primaryColor': persona.primaryColor, '--secondaryColor': persona.secondaryColor, '--backgroundColor': persona.backgroundColor } as React.CSSProperties} className={styles.RootWrapper}>
+            <img src={persona.iconSrc} alt={persona.displayName} className={styles.Icon} width={128} height={128}/>
+            <div className={styles.TextAreaWrapper}>
+                <h2>{persona.displayName}</h2>
+                <p>{persona.description.length > 30 ? persona.description.substring(0, MAX_PERSONA_DESC_LENGTH-1) + "…" : persona.description}</p>
+            </div>
+        </main>
+    )
+}
+
