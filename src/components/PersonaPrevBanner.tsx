5e2a612ced601b12fa42bceabc6ce5398ff40b6c
NitCelcius <76618755+NitCelcius@users.noreply.github.com>
2024-03-11 10:22:42 +0900

    WIP: icons for persona

--- src/components/PersonaPrevBanner.tsx
+++ src/components/PersonaPrevBanner.tsx
@@ -3,15 +3,21 @@ import {Persona} from "@/types/Persona";
 import styles from "./PersonaPrevBanner.module.css";
 import personaStyles from "./PersonaShared.module.css";
 
-import {MAX_PERSONA_DESC_LENGTH} from "@/components/MAX_PERSONA_DESC_LENGTH";
+import {MAX_PERSONA_BANNER_LENGTH, MAX_PERSONA_DESC_LENGTH} from "@/components/MAX_PERSONA_DESC_LENGTH";
 
 export default function PersonaPrevBanner(persona: Persona) {
     return (
-        <main style={{ '--primaryColor': persona.primaryColor, '--secondaryColor': persona.secondaryColor, '--backgroundColor': persona.backgroundColor } as React.CSSProperties} className={styles.RootWrapper}>
-            <img src={persona.iconSrc} alt={persona.displayName} className={styles.Icon} width={128} height={128}/>
+        <main style={{
+            '--primaryColor': persona.primaryColor,
+            '--secondaryColor': persona.secondaryColor,
+            '--backgroundColor': persona.backgroundColor
+        } as React.CSSProperties} className={styles.RootWrapper}>
+            <img src={persona.iconSrc} alt={persona.displayName} className={`${styles.Icon} ${personaStyles.Icon}`}
+                 width={128}
+                 height={128}/>
             <div className={styles.TextAreaWrapper}>
                 <h2>{persona.displayName}</h2>
-                <p>{persona.description.length > 30 ? persona.description.substring(0, MAX_PERSONA_DESC_LENGTH-1) + "…" : persona.description}</p>
+                <p>{persona.description.length > MAX_PERSONA_BANNER_LENGTH ? persona.description.substring(0, MAX_PERSONA_BANNER_LENGTH - 1) + "…" : persona.description}</p>
             </div>
         </main>
     )
