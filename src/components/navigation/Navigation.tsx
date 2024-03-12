504d110aa307c05c93ef32ec8c5f5814a58edac2
tererun <27857171+tererun@users.noreply.github.com>
2024-03-12 12:14:18 +0900

    feat: impl dark-theme

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -6,17 +6,23 @@ import Image from "next/image";
 import NavigationPageLink from "@/components/navigation/NavigationPageLink";
 import NavigationExternalLink from "@/components/navigation/NavigationExternalLink";
 import { usePathname } from 'next/navigation'
+import {useEffect, useState} from "react";
 
 const Navigation = () => {
 
-    const pathname = usePathname()
+    const pathname = usePathname();
+    const [darkTheme, setDarkTheme] = useState<boolean>(false);
+
+    useEffect(() => {
+        setDarkTheme(window.matchMedia('(prefers-color-scheme: dark)').matches);
+    }, []);
 
     return (
         <nav className={styles.Navigation}>
             <div className={styles.NavigationTop}>
                 <div className={styles.Logo}>
                     <Link href={"/"}>
-                        <Image className={styles.LogoImage} src={"/logo_light.svg"} alt={"TNP Logo"} width={2880} height={1080} />
+                        <Image className={styles.LogoImage} src={darkTheme ? "/logo_dark.svg" : "/logo_light.svg"} alt={"TNP Logo"} width={2880} height={1080} />
                     </Link>
                 </div>
                 <div className={styles.Links}>
