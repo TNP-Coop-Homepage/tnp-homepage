23e620a3d0b3120ec75dee586e8afd10f6b8d96e
Reiru329411 <running941293@gmail.com>
2024-03-25 21:47:31 +0900

    Change game images
    ゲーム画面のスクショに変更

--- src/app/workIntroduction/WORK_CONTENTS.ts
+++ src/app/workIntroduction/WORK_CONTENTS.ts
@@ -1,51 +1,48 @@
 import { GamePreviewImage } from "@/types/GamePreviewImage"
 import { Work } from "@/types/Work"
 
-const images: GamePreviewImage[] = [
+const CorelynxImages: GamePreviewImage[] = [
     {
-        imageSrc: "/sampleA.png",
-        displayName: "A",
-        width: 440,
-        height: 310
+        imageSrc: "/Corelynx1.webp",
+        displayName: "Corelynx1",
+        width: 768,
+        height: 432
     },
     {
-        imageSrc: "/sampleB.png",
-        displayName: "B",
-        width: 440,
-        height: 310
-    },
-    {
-        imageSrc: "/sampleC.png",
-        displayName: "C",
-        width: 440,
-        height: 310
-    },
-    {
-        imageSrc: "/sampleD.png",
-        displayName: "D",
-        width: 440,
-        height: 310
-    },
-    {
-        imageSrc: "/sampleE.png",
-        displayName: "E",
-        width: 440,
-        height: 310
+        imageSrc: "/Corelynx2.webp",
+        displayName: "Corelynx2",
+        width: 768,
+        height: 432
     }
 ]
 
 const Corelynx: Work = {
     title: "Corelynx",
     authors: ["空中ブランコ"],
-    images: images,
+    images: CorelynxImages,
     explanation: "試合ごとに地形が変わる世界で Capture The Flag で遊ぶオンラインマルチプレイFPS。コアを見つけ占領し、敵から守り抜こう！",
     comment: null
 }
 
+const VergiliusImages: GamePreviewImage[] = [
+    {
+        imageSrc: "/Vergilius1.webp",
+        displayName: "Vergilius1",
+        width: 768,
+        height: 432
+    },
+    {
+        imageSrc: "/Vergilius2.webp",
+        displayName: "Vergilius2",
+        width: 768,
+        height: 432
+    }
+]
+
 const Vergilius: Work = {
     title: "Vergilius",
     authors: ["Δmp", "レイル", "℃(せし)", "yoshinob", "yugo"],
-    images: null,
+    images: VergiliusImages,
     explanation: "5人で合作した作品のプロトタイプ\nランダムに生成されるダンジョンを進んでいくローグライク。インベントリから魔法を組み替えてオリジナルの魔法を作り、深みを目指そう！",
     comment: null
 }
