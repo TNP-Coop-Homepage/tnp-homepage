fcb6d905eaa2a1716c0695099391d6400747b934
NitCelcius <76618755+NitCelcius@users.noreply.github.com>
2024-03-27 23:37:22 +0900

    Disable PWA feature for dev build
    開発中は PWA の機能を使うことはないと判断し、開発中のみ同機能を無効としました

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ import nextPWA from "next-pwa";
 /** @type {import("next-pwa").PWAConfig} */
 const nextConfig = nextPWA({
   dest: "public",
-  // disable: process.env.NODE_ENV === "development",
+  disable: process.env.NODE_ENV === "development",
 });
 
 nextConfig.reactStrictMode = true;
