f15544feefd670d3dc3251792108c6e3b583096c
onorp <81564314+onorp@users.noreply.github.com>
2025-01-17 15:31:43 +0900

    feat: support mobile

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -5,38 +5,50 @@ import Link from "next/link";
 import Image from "next/image";
 import NavigationPageLink from "@/components/navigation/NavigationPageLink";
 import NavigationExternalLink from "@/components/navigation/NavigationExternalLink";
-import { usePathname } from 'next/navigation'
-import { CONTACT_FORM_URL } from '@/consts/CONTACT_FORM_URL';
+import { useState } from "react";
+import { usePathname } from "next/navigation";
+import { CONTACT_FORM_URL } from "@/consts/CONTACT_FORM_URL";
+import NavigationLink from "@/components/navigation/NavigationLink";
 
 const Navigation = () => {
 
     const pathname = usePathname()
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleLinkClick = () => setIsOpen(false);
 
     return (
-        <nav className={styles.Navigation}>
-            <div className={styles.NavigationTop}>
-                <div className={styles.Logo}>
-                    <Link href={"/"}>
-                        <Image className={styles.LogoImage} src={"/logo_light.svg"} alt={"TNP Logo"} width={2880} height={1080} />
-                    </Link>
-                </div>
-                <div className={styles.Links}>
-                    <NavigationPageLink title={"Home"} selected={pathname === "/"} url={"/"} />
-                    <NavigationPageLink title={"About"} selected={pathname === "/about"} url={"/about"} />
-                    <NavigationExternalLink title={"Blog"} url={"http://akitatnp.wp.xdomain.jp/"} />
-                    <NavigationPageLink title={"Works"} selected={pathname === "/works"} url={"/works"} />
-                    <NavigationExternalLink title={"Contact"} url={CONTACT_FORM_URL} />
-                </div>
+        <>
+            <div className={styles.Hamburger} onClick={() => setIsOpen(!isOpen)} style={{ backgroundColor: isOpen ? "transparent" : "#00000055" }}>
+                {isOpen ? "×" : "≡"}
             </div>
-            <div className={styles.NavigationBottom}>
-                <a href="https://twitter.com/tnp_akita">
-                    <img className={styles.X} src="/X-logo.svg" alt="X" />
-                </a>
-                <p>
-                    © 2024 TNP
-                </p>
-            </div>
-        </nav>
+            <nav className={styles.Navigation} style={{ left: isOpen ? 0 : "-100%" }}>
+                <div className={styles.NavigationTop}>
+                    <div className={styles.Logo}>
+                        <Link href={"/"}>
+                            <Image className={styles.LogoImage} src={"/logo_light.svg"} alt={"TNP Logo"} width={2880} height={1080} />
+                        </Link>
+                    </div>
+                    <NavigationLink onClick={handleLinkClick}>
+                        <div className={styles.Links}>
+                            <NavigationPageLink title={"Home"} selected={pathname === "/"} url={"/"} />
+                            <NavigationPageLink title={"About"} selected={pathname === "/about"} url={"/about"} />
+                            <NavigationExternalLink title={"Blog"} url={"http://akitatnp.wp.xdomain.jp/"} />
+                            <NavigationPageLink title={"Works"} selected={pathname === "/works"} url={"/works"} />
+                            <NavigationExternalLink title={"Contact"} url={CONTACT_FORM_URL} />
+                        </div>
+                    </NavigationLink>
+                </div>
+                <div className={styles.NavigationBottom}>
+                    <a href="https://twitter.com/tnp_akita">
+                        <img className={styles.X} src="/X-logo.svg" alt="X" />
+                    </a>
+                    <p>
+                        © 2025 TNP
+                    </p>
+                </div>
+            </nav>
+        </>
     );
 };
 
